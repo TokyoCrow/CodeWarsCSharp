238c5f5b19644956c4c8f8456f87d217a37c62d3
Tokyo <emil123904@gmail.com>
2020-09-22 01:01:03 +0300

    Fourth day

---
+++ CodeWarsSCharp/Intervals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    //https://www.codewars.com/kata/52b7ed099cdc285c300001cd
+    public static class Intervals
+    {
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            var sum = new List<int>();
+            foreach ((int, int) interval in intervals)
+                for (var i = interval.Item1 + 1; i <= interval.Item2; i++)
+                    sum.Add(i);
+            return sum.Distinct().Count();
+        }
+    }
+}
